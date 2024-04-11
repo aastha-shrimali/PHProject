9397789ec7f99edaa74216f76fe52be08ff0b11a
5aast <5aast@10.0.0.140>
2024-04-10 20:43:14 -0400

    TestDummy1

---
+++ LoginFunctions.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php 
+session_start();
+?>
+    <style>
+        h2{
+            margin-left: 25px
+        }
+        p{
+            white-space: pre-line; 
+            margin: 25px
+        }
+    </style>
+        
+        <h1>Welcome to Algonquin Social Media Website</h1>
+    <p>If you have never used this before, you have to <a href="NewUser.php">Sign Up</a> first.</p>
+    <p>If you have already signed up, you can <a href="Login.php">Log In</a> now.</p>
+    <div>
+        
+        
+    </div>
